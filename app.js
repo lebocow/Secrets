1b3b8128232822cfb95ee8c60e1282a13841f09a
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2022-09-21 18:37:23 +0300

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,78 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.mongoDBConn);
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.route("/").get(function (req, res) {
+  res.render("home");
+});
+
+app
+  .route("/register")
+  .get(function (req, res) {
+    res.render("register");
+  })
+  .post(function (req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+    newUser.save(function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+
+app
+  .route("/login")
+  .get(function (req, res) {
+    res.render("login");
+  })
+  .post(function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function (err, foundUser) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+          } else {
+            res.send("Incorrect username or password. Try again");
+          }
+        }
+      }
+    });
+  });
+
+app.listen(PORT, function () {
+  console.log("Server is running on port 3000");
+});
