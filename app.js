6b998bcdf0ba0df5860001d1cbde31b374822d8d
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2022-09-23 22:10:57 +0300

    Tweaked secret

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "secret",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
   })
